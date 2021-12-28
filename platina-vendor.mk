9463d413f37eed0743fd8ba648248d8c4eceedce
daniml3 <danimoral1001@gmail.com>
2021-12-28 23:07:19 +0700

    platina: Drop QTI datastatusnotification
    * In some situations this app crashes due to a missing framework
      method on A12, causing network fluctuation with dual sim.
    
    * As it isn't needed, let's just remove it
    
    Signed-off-by: daniml3 <danimoral1001@gmail.com>
    Signed-off-by: Hanif Ardhani <hanif.ardhani98@gmail.com>
    Change-Id: I312522b8f7cd3b2541f9466d55e51199844bad7e

--- platina-vendor.mk
+++ platina-vendor.mk
@@ -1101,7 +1101,6 @@ PRODUCT_PACKAGES += \
     TimeService \
     PowerOffAlarm \
     QtiTelephonyService \
-    datastatusnotification \
     imssettings \
     uceShimService \
     dpmserviceapp \
